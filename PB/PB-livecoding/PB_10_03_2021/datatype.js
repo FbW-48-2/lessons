fadc60c39f99370a53c8992f5045e45e07400ff5
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2021-03-10 13:50:28 +0100

    Data Types pt 2

--- PB/PB-livecoding/PB_10_03_2021/datatype.js
+++ PB/PB-livecoding/PB_10_03_2021/datatype.js
@@ -8,21 +8,72 @@ Data Types: An introduction
 
 //!Primitive Data Types: Numbers, Strings, Boolean, Null and Undefined
 
-//A- numbers ==> float and integer
+//1- Number ==> float and integer
 let intNumber = 100
 let floatNumber = 5.44
+let num = 44  
 
-// Number 
-let num = 44 ;  // Number 
-let num2 = '44';  // String
+console.log(intNumber);
+/* ################ */
+//2- String
+
+let str_1 = 'hallo world'
+let str_2 = "I like Js"
+let num2 = '44'
+
+console.log(str_2)
+
+/** ############ */
+//3- Boolean ==> true or false
+
+let isAdmin = true
+let isOpen = false
+
+/** ############# */
+
+//4 - Null ==> empty 
+
+let name = null
+
+console.log(name);
+/** ############# */
+//5- Undefined ==> undefined
+
+let und = undefined
+
+console.log(und)
+
+/** ############# */
+//6- Array ==> [item_1, item_2,...] ==> complex ??
+let arrOfNames = ['Alex','Jon',100,null,undefined, name]
+
+console.log(arrOfNames)
+/**
+ [ 'Alex', 'Jon', 100, null, undefined, null ]
+ */
+
+
+/** ############# */
+//7- Object ==> { key: value} ==> complex ??
+
+let person = { name:'Max', age:30}
+
+
+/** ###################### */
+
+console.log(typeof 100) // number
+console.log(person); // { name: 'Max', age: 30 }
+console.log(typeof person); // object
+
+console.log(person.name) // Max
+/*
 let userName; 
 userName = 'Alex'
 userName = "Max"
 console.log(typeof  num  );
 console.log(typeof num2  );
 console.log(userName);
-
-
+*/
 
 
 
