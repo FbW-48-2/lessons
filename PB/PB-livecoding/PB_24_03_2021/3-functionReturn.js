a41dc5a6cf9f22a519e78fbb12824dedf57e5db9
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2021-03-24 08:47:10 +0100

    functions update the templates

---
+++ PB/PB-livecoding/PB_24_03_2021/3-functionReturn.js
@@ -0,0 +1,3 @@
+// A return statement determines the value the function returns.
+// A return keyword without an expression after it will cause the function to return undefined.
+// Functions that don’t have a return statement at all return undefined.
