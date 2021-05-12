599e5e4e24c46c55b9b11deed952d2af2f723ef1
vasilis <skrvasilis@gmail.com>
2021-05-12 15:31:09 +0200

    Functions-recursion-3-solution

---
+++ PB/PB-solutions/PB-Functions-Recursion-3-solution/index.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+    if (str === "") // This is the terminal case that will end the recursion
+      return "";
+    
+    else
+      return reverseString(str.substr(1)) + str.charAt(0);
+}
+
+console.log(reverseString("hello world"))
